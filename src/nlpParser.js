56c5314ec311aed894ad7fc5304ff08110339e77
Giri <121802870+Girii4@users.noreply.github.com>
2026-03-28 10:41:46 +0530

    Update src/nlpParser.js to export parseQuery function

--- src/nlpParser.js
+++ src/nlpParser.js
@@ -1,61 +1,9 @@
-// nlpParser.js
+// Exporting parseQuery function
 
-/**
- * Natural Language Processing Parser for SQL Components
- */
-
-class NLPParser {
-    constructor(query) {
-        this.query = query;
-        this.components = {
-            action: '',
-            table: '',
-            columns: [],
-            conditions: [],
-            orderBy: '',
-            limit: '',
-            joins: []
-        };
-    }
-
-    extractComponents() {
-        this.extractAction();
-        this.extractTable();
-        this.extractColumns();
-        this.extractConditions();
-        this.extractOrderBy();
-        this.extractLimit();
-        this.extractJoins();
-    }
-
-    extractAction() {
-        // Logic to extract action (SELECT, INSERT, etc.) from the query
-    }
-
-    extractTable() {
-        // Logic to extract table name from the query
-    }
-
-    extractColumns() {
-        // Logic to extract column names from the query
-    }
-
-    extractConditions() {
-        // Logic to extract conditions from the query
-    }
-
-    extractOrderBy() {
-        // Logic to extract order by clauses from the query
-    }
-
-    extractLimit() {
-        // Logic to extract limit from the query
-    }
-
-    extractJoins() {
-        // Logic to extract joins from the query
-    }
+function parseQuery(query) {
+    // Implement query parsing logic here
+    // Return parsed components as an object
+    return {};
 }
 
-// Export the NLPParser class
-module.exports = NLPParser;
+module.exports = { parseQuery };
